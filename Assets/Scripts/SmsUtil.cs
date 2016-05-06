052b3ff7b2842c046988a62157a8455f398aff9a
kanontang <kanontang@gmail.com>
2016-05-06 23:58:05 +0800

    包装安卓设备的短信发送功能。

---
+++ Assets/Scripts/SmsUtil.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 短信工具
+/// </summary>
+public class SmsUtil
+{
+    //安卓类
+#if UNITY_ANDROID
+    private static AndroidJavaClass ajc = null;
+#endif
+
+    /// <summary>
+    /// 发送短信
+    /// </summary>
+    /// <param name="number">号码</param>
+    /// <param name="content">内容</param>
+    public static void smsSend(string number, string content)
+    {
+#if UNITY_ANDROID
+        if (ajc == null)
+            ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        ajo.Call("smsSend", new string[] { number, content });
+#elif UNITY_IPHONE
+        //TODO
+#endif
+    }
+    
+    /// <summary>
+    /// 打开短信
+    /// </summary>
+    /// <param name="number">号码</param>
+    /// <param name="content">内容</param>
+    public static void openSms(string number, string content)
+    {
+#if UNITY_ANDROID
+        if (ajc == null)
+            ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        ajo.Call("openSms", new string[] { number, content });
+#elif UNITY_IPHONE
+        //TODO
+#endif
+    }
+}
---
+++ Assets/Scripts/SmsUtilTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SmsUtilTest : MonoBehaviour
+{
+    public InputField numberText;
+    public InputField contentText;
+    public Button sendBtn;
+    public Button openBtn;
+    // Use this for initialization
+    void Start()
+    {
+        this.sendBtn.onClick.AddListener(sendBtnClickHandler);
+        this.openBtn.onClick.AddListener(openBtnClickHandler);
+    }
+
+    private void openBtnClickHandler()
+    {
+        if (this.numberText.text == "" ||
+            this.contentText.text == "") return;
+        SmsUtil.openSms(this.numberText.text, this.contentText.text);
+    }
+
+    private void sendBtnClickHandler()
+    {
+        if (this.numberText.text == "" || 
+            this.contentText.text == "") return;
+        SmsUtil.smsSend(this.numberText.text, this.contentText.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
